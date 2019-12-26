2a9eef2c3490f3c8062a2fd259ec10289266bc0f
mac <ghayoor10th@gmail.com>
2019-12-26 09:05:20 +0500

    center icon circles created

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
 
-  Widget _container (Color color){
-
+  Widget _container (Color color, EdgeInsets margin ,icon){
+      return Container(
+                margin: margin,
+                height: 60.0,
+                width: 60.0,
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(50.0),
+                  color: color
+                ),
+                child: new Icon(icon, color: Colors.white,),
+              );
   }
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,15 +47,11 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Stack(
+              alignment: Alignment.center,
               children: <Widget>[
-                new Container(
-                height: 60.0,
-                width: 60.0,
-                decoration: new BoxDecoration(
-                  borderRadius: new BorderRadius.circular(50.0),
-                  color: Colors.orange
-                ),
-              )
+                _container(Colors.orange[200], EdgeInsets.only(right: 50.0) ,Icons.place,),
+                _container(Colors.orange[400], EdgeInsets.only(top:70.0), Icons.tag_faces,),
+                _container(Colors.orange[700], EdgeInsets.only(left:50.0), Icons.rate_review,),
               ],
             )
           ],
